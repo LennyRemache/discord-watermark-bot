cb162230e06fa4eea2bffcc0e2fc06dfa067b240
LennyRemache <lenny.j.remache@gmail.com>
2024-09-13 19:34:29 -0400

    Modified email command to only be used by admins

--- src/index.js
+++ src/index.js
@@ -1,25 +1,35 @@
-import { Client, GuildMember, IntentsBitField } from "discord.js";
+import { Client, GuildMember, IntentsBitField, EmbedBuilder } from "discord.js";
 import dotenv from "dotenv";
 dotenv.config();
 
 const client = new Client({
   intents: [
     IntentsBitField.Flags.Guilds,
-    IntentsBitField.Flags.GuildMembers,
     IntentsBitField.Flags.GuildMessages,
     IntentsBitField.Flags.MessageContent,
-    IntentsBitField.Flags.DirectMessages,
+    IntentsBitField.Flags.GuildMembers,
   ],
 });
 
+// signal when bot is live
 client.on("ready", (c) => {
   console.log(`${c.user.tag} is online!`);
 });
 
+// custom prefix commands
 client.on("messageCreate", (message) => {
   console.log(message.content);
+  if (message.content.startsWith("?")) {
+    const command = message.content.slice(1);
+
+    if (command === "email") {
+      console.log(message.member.permissions.has("Administrator"));
+      message.channel.send("EMAIL");
+    }
+  }
 });
 
+// slash commands
 client.on("interactionCreate", (interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
