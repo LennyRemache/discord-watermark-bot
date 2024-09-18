52373398f149436dcfbe3f887d6650fcddf6ffaf
LennyRemache <lenny.j.remache@gmail.com>
2024-09-17 21:56:18 -0400

    Added an exclamation point to bot message

--- src/index.js
+++ src/index.js
@@ -57,7 +57,7 @@ client.on("messageCreate", async (message) => {
         .setImage("attachment://watermarked-image.png");
 
       await message.channel.send({
-        content: `Congratulationso on your success <@${user.id}> `,
+        content: `Congratulationso on your success <@${user.id}>!`,
         embeds: [embed],
         files: [processedAttachment],
       });
